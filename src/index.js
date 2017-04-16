bd7f6c725bdb0f91a6883f19be8222e09ebb2a98
PiotrBerebecki <petebere@gmail.com>
2017-04-16 06:56:03 +0100

    Add redux packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Provider, createStore } from 'redux';
+// import { name } from 'module';
 
 import App from './App';
 import './index.css';
 
+
+// const storewithMiddleWare =
+
+
 ReactDOM.render(
-  <App />,
+  // <Provider store={}>
+    <App />
+  // </Provider>,
+  //
+  //
+,
   document.getElementById('root')
 );
